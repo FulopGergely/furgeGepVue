1055ff677939dfc277a8f5556e05df7789e93214
Gergo <fgergo91@gmail.com>
2021-09-23 08:38:59 +0200

    first commit

---
+++ src/views/Gepkolcsonzes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="gepkolcsonzes">
+    <Gyalu msg="Welcome to Your Gyalu" />
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import Gyalu from "@/components/Gyalu.vue";
+
+export default {
+  name: "gyalu",
+  components: {
+    Gyalu,
+  },
+};
+</script>
