58313dc45dbff2591f3052e18c84db763b18bb09
FulopGergely <48963322+FulopGergely@users.noreply.github.com>
2022-03-04 09:49:38 +0100

    Update table.vue

--- src/views/table.vue
+++ src/views/table.vue
@@ -25,9 +25,9 @@ export default {
           name: "Ácsgyalu",
           discription: "Teljesítmény: 1 200 W Teljes hossz: 525 mm Lökésszám: 15 000 / perc Gyalulási szélesség: 170 mm Vágásvastagság: 2 mm Méret (hossz x szélesség x magasság): 530 mm x 240 mm x 150 mm Súly: 9 kg",
           link: "",
-          price1: 6500,
-          price2: 6100,
-          price3: 5800
+          price1: 7000,
+          price2: 6600,
+          price3: 6300
         },
         {
           name: "Merülő körfűrész",
